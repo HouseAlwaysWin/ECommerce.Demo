dcdc221c0eeb01e6c6da7e44f7a67f2e4cccbca2
MartinWang <martinwang7963@gmail.com>
2020-01-05 23:10:44 +0800

    Add localize veevalidate

--- ECommerce.Presentation/wwwroot/js/account/register.js
+++ ECommerce.Presentation/wwwroot/js/account/register.js
@@ -1,5 +1,28 @@
 Vue.component("validation", VeeValidate.ValidationProvider);
 
+// install localize
+VeeValidate.localize({
+  zh_TW: {
+    names: {
+      email: "電子郵件",
+      password: "密碼",
+      confirmPassword: "密碼再確認"
+    },
+    messages: zh_TW.messages
+  }
+});
+// use current lang
+VeeValidate.localize("zh_TW");
+
+Vue.use(VeeValidate);
+
+const i18n = new VueI18n({
+  locale: "zh_TW",
+  zh_TW: {
+    validations: zh_TW
+  }
+});
+
 var registerVue = new Vue({
   el: "#registerVue",
   data: {
